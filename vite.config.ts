02cc25cd88bb9b81828c6b3daaf96b6826202ae1
Damon Alexander White <damonaw@gmail.com>
2025-10-29 09:03:42 -0600

    Fix base paths for portable builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const repositoryBase = '/crackle-date-2/'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? repositoryBase : '/',
+  base: command === 'build' ? './' : '/',
   plugins: [react()],
   server: {
     host: true, // or '0.0.0.0'
-    port: 5173
-  }
-}))
+    port: 5173,
+  },
+}));
